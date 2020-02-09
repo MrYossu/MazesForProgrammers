261407f3a0cebdf12c060fa828edac54fe5e8bed
AY Silver <mryossu@hotmail.com>
2020-02-09 18:58:35 +0000

    Added a Shuffle method to the Extensions class, and used it to simplify the definition of Rand

--- Mazes.Models/Models/Extensions.cs
+++ Mazes.Models/Models/Extensions.cs
@@ -15,7 +15,10 @@ namespace Mazes.Models.Models {
     }
 
     public static T Rand<T>(this IEnumerable<T> items, Random r = null) =>
-      items.Skip((r ?? new Random((int)DateTime.Now.Ticks)).Next(1000) % items.Count()).First();
+      items.Shuffle(r).First();
+
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> items, Random r = null) =>
+      items.OrderBy(n => (r ?? new Random()).Next());
 
     public static TReturn Switch<TCase, TReturn>(this IEnumerable<(TCase thisCase, Func<TReturn> f)> cases, TCase theCase, TReturn defaultValue = default) {
       foreach ((TCase thisCase, Func<TReturn> f) c in cases) {
